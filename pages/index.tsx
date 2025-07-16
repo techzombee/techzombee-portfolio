384b1c44309b749c30aba213851ab47cf69db282
HEECHAN CHO <wkfyd6263@gmail.com>
2025-07-16 11:21:27 +0900

    feat: show sns icons on mainpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { supabase } from '../lib/supabase'
 import AuthGuard from '../components/AuthGuard'
+import SnsIcons from '@/components/snsIcons'
 
 export default function Home() {
   const router = useRouter()
@@ -20,18 +21,24 @@ export default function Home() {
   }
 
   return (
-    <AuthGuard>
-        <main className="min-h-screen flex flex-col items-center justify-center p-4">
-        <h1 className="text-2xl font-bold text-green-600 mb-4">
-            Welcome{userEmail ? `, ${userEmail}` : ''}!
-        </h1>
-        <button
-            onClick={handleLogout}
-            className="bg-red-600 text-white px-4 py-2 rounded"
-        >
-            Log out
-        </button>
-        </main>
-    </AuthGuard>
+    <>
+      <div className="flex justify-center mt-4">
+        <SnsIcons />
+      </div>
+      <AuthGuard>
+          <main className="min-h-screen flex flex-col items-center justify-center p-4">
+            
+            <h1 className="text-2xl font-bold text-green-600 mb-4">
+                Welcome{userEmail ? `, ${userEmail}` : ''}!
+            </h1>
+            <button
+                onClick={handleLogout}
+                className="bg-red-600 text-white px-4 py-2 rounded"
+            >
+                Log out
+            </button>
+          </main>
+      </AuthGuard>
+    </>
   )
 }
